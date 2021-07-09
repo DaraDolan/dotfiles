1d5bb1ab5a42ba2ac21f44040905be5cae94ab30
Dara Dolan <daradolan@gmail.com>
2021-07-09 19:22:32 +0100

    Modifications to vim

---
+++ zsh/local/vim.zsh
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+# Aliases and functions for Vim
+# ------------------------------------------------------------------------------
+
+# Set default editor
+export EDITOR='nvim'
+
+# Open vim with z argument
+v() {
+  if [ -n "$1" ]; then
+	  z $1
+  fi
+
+  $EDITOR
+}
