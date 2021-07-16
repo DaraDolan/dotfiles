a92e4a27db151a7209de4a7b1211959bebcc9f31
Dara Dolan <daradolan@gmail.com>
2021-07-16 10:57:40 +0100

    Added some telescope setup

---
+++ vim/config/telescope.lua
@@ -0,0 +1,62 @@
+--------------------------------------------------------------------------------
+-- Telescope Config
+--------------------------------------------------------------------------------
+
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+
+telescope.setup {
+  defaults = {
+    prompt_prefix = '  ',
+    sorting_strategy = "ascending",
+    -- layout_strategy = "telescopic_jersey",
+    layout_config = {
+      prompt_position = "top",
+    },
+    mappings = {
+      i = {
+        ["<Esc>"] = actions.close,
+        ["<C-q>"] = actions.send_to_qflist,
+      },
+    },
+    file_ignore_patterns = { 'node_modules' },
+    -- vimgrep_arguments = {'ag', '--nogroup', '--column'},
+  },
+  pickers = {
+    find_files = {
+      prompt_title = 'All Files',
+      find_command = {'rg', '--files', '--no-ignore', '--hidden'},
+    },
+    current_buffer_fuzzy_find = {
+      prompt_title = 'Current Buffer Lines',
+    },
+    oldfiles = {
+      prompt_title = 'History',
+    },
+  },
+}
+
+-- telescope.load_extension('fzf')
+telescope.load_extension('sourcery')
+telescope.load_extension('ultisnips')
+
+
+--------------------------------------------------------------------------------
+-- Custom Finders
+--------------------------------------------------------------------------------
+
+local builtin = require('telescope.builtin')
+
+builtin.dotfiles = function ()
+  builtin.find_files({
+    prompt_title = 'Dotfiles',
+    cwd = "$HOME/.dotfiles",
+  })
+end
+
+builtin.project_history = function ()
+  builtin.oldfiles({
+    prompt_title = 'Project History',
+    cwd_only = true,
+  })
+end
